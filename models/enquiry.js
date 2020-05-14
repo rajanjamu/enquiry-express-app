7935db7d5df03f1d2126a466fdfe5d0ec246dca1
rajanjamu <rajanjamu@gmail.com>
2020-05-14 16:44:50 +0530

    enquiry model

--- models/enquiry.js
+++ models/enquiry.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose')
 
 const enqSchema = new mongoose.Schema({
+    enquiryNumber: { type: Number, default: 1 },
+    dseName: String,
     prospectname: String,
-    mobileNumber: String
+    address: String,
+    mobileNumber: String,
+    modelName: String,
+    fuelType: { 
+        type: String, 
+        enum: ['Petrol', 'Diesel']
+    },
+    variantName: { 
+        type: String, 
+        enum: ['Sigma', 'Delta', 'Zeta', 'Alpha'] 
+    },
+    enquiryStatus: { 
+        type: String, 
+        enum: ['Cold', 'Warm', 'Hot', 'Lost', 
+            'Order', 'Invoiced', 'Postponed'] 
+    },
+    source: String,
+    buyerType: {
+        type: String,
+        enum: ['First Time', 'Additional', 'Replacement']
+    },
+    lostReason: String,
+    testDrive: { status: Boolean, date: Date },
+    followUp: [{
+        date: Date,
+        remarks: String,
+        nextDate: Date
+    }],
 }, {
     timestamps: true
 })
